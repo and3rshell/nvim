aad03f2f10b7ee5376194433f0982385897948d1
prospero <g0lden_vo1d@protonmail.com>
2023-04-10 19:38:12 +0200

    feat: tweaks

--- lua/prospero/vimwiki.lua
+++ lua/prospero/vimwiki.lua
@@ -1,5 +1,5 @@
 vim.cmd [[
-  let g:vimwiki_list = [{'path': '~/doc/vimwiki/', 'syntax': 'default', 'ext': '.md'}]
+  let g:vimwiki_list = [{'path': '~/doc/vimwiki/', 'syntax': 'markdown', 'ext': '.md'}]
   let g:vimwiki_url_maxsave = 0
   let g:vimwiki_auto_chdir = 1
 ]]
