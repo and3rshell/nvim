8a939cdb076b1fd89c52cf939ec6a35733cd5003
prospero <g0lden_vo1d@protonmail.com>
2023-05-06 19:40:17 +0200

    nvim

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<C-t>", ':FloatermToggle<CR>')
-vim.keymap.set("n", "<C-e>", ':Lf<CR>')
+vim.keymap.set("n", "<C-e>", '<C-\\><C-n>:Lf<CR>')
+vim.keymap.set("n", "<C-g>", '<C-\\><C-n>:FloatermNew lazygit<CR>')
 vim.keymap.set("t", "<C-t>", '<C-\\><C-n>:FloatermToggle<CR>')
