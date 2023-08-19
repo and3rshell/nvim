d4fd2be2024ae26b57e644955466cc221ed3aefa
prospero <g0lden_vo1d@protonmail.com>
2023-08-19 20:15:42 +0200

    sync

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -48,7 +48,8 @@ require'nvim-treesitter.configs'.setup {
 
 	matchup = {
 		enable = true, -- mandatory, false will disable the whole extension
-		disable = { "javascript" }, -- optional, list of language that will be disabled
+		disable = { }, -- optional, list of language that will be disabled
+		-- disable = { "javascript" }, -- optional, list of language that will be disabled
 		-- options
 	},
 }
--- lua/prospero/packer.lua
+++ lua/prospero/packer.lua
@@ -35,82 +35,83 @@ packer.init {
 }
 
 return packer.startup(function(use)
-    use("wbthomason/packer.nvim")
-    use("nvim-lua/plenary.nvim")
-    use("nvim-telescope/telescope.nvim")
-    use("nvim-lua/popup.nvim")
-    use("lewis6991/impatient.nvim")
-    use("windwp/nvim-autopairs")
-    use("ThePrimeagen/harpoon")
-    use("nvim-lualine/lualine.nvim")
-    use("vimwiki/vimwiki")
-    use("mbbill/undotree")
-    use("folke/zen-mode.nvim")
+    use { "wbthomason/packer.nvim", commit = "90f794d01dd5f50404f8ef6e1d22377ba5180787" }
+    use { "nvim-lua/plenary.nvim", commit = "267282a9ce242bbb0c5dc31445b6d353bed978bb" }
+    use { "nvim-telescope/telescope.nvim", commit = "2d92125620417fbea82ec30303823e3cd69e90e8" }
+    use { "nvim-lua/popup.nvim", commit = "b7404d35d5d3548a82149238289fa71f7f6de4ac" }
+    use { "windwp/nvim-autopairs", commit = "" }
+    use { "ThePrimeagen/harpoon", commit = "" }
+    use { "nvim-lualine/lualine.nvim", commit = "" }
+    use { "vimwiki/vimwiki", commit = "" }
+    use { "mbbill/undotree", commit = "" }
+    use { "folke/zen-mode.nvim", commit = "" }
 
     -- colors and icons
-    use("lunarvim/darkplus.nvim")
-    use("kyazdani42/nvim-web-devicons")
+    use { "lunarvim/darkplus.nvim", commit = "" }
+    use { "kyazdani42/nvim-web-devicons", commit = "" }
     -- use("nvim-tree/nvim-tree.lua")
     -- use("nvim-tree/nvim-web-devicons")
-    use("tamago324/lir.nvim")
-    use("tamago324/lir-git-status.nvim")
+    use { "tamago324/lir.nvim", commit = "" }
+    use { "tamago324/lir-git-status.nvim", commit = "" }
 
     -- treesitter and related
-    use("nvim-treesitter/nvim-treesitter", { run = ':TSUpdate' })
-    use("nvim-treesitter/nvim-treesitter-context");
-    use("andymass/vim-matchup")
-    use("numToStr/Comment.nvim")
-    use("JoosepAlviste/nvim-ts-context-commentstring")
-    use("tpope/vim-surround") -- ex. cs"', ds", ysiw]
+    use { "nvim-treesitter/nvim-treesitter", { run = ':TSUpdate' }, commit = "" }
+    use { "nvim-treesitter/nvim-treesitter-context", commit = "" }
+    -- use("andymass/vim-matchup")
+    use { "numToStr/Comment.nvim", commit = "" }
+    use { "JoosepAlviste/nvim-ts-context-commentstring", commit = "" }
+    use { "tpope/vim-surround", commit = "" } -- ex. cs"', ds", ysiw]
 
     -- lsp
-    use("neovim/nvim-lspconfig")
-    use("williamboman/mason.nvim")
-    use("williamboman/mason-lspconfig.nvim")
+    use { "neovim/nvim-lspconfig", commit = "" }
+    use { "williamboman/mason.nvim", commit = "" }
+    use { "williamboman/mason-lspconfig.nvim", commit = "" }
     -- use("onsails/lspkind.nvim")
     -- use("ray-x/lsp_signature.nvim")
 
     -- cmp
-    use("hrsh7th/nvim-cmp")
-    use("hrsh7th/cmp-buffer")
-    use("hrsh7th/cmp-path")
-    use("saadparwaiz1/cmp_luasnip")
-    use("hrsh7th/cmp-cmdline")
-    use("hrsh7th/cmp-nvim-lsp")
-    use('hrsh7th/cmp-nvim-lua')
-    use('jezda1337/cmp_bootstrap')
+    use { "hrsh7th/nvim-cmp", commit = "" }
+    use { "hrsh7th/cmp-buffer", commit = "" }
+    use { "hrsh7th/cmp-path", commit = "" }
+    use { "saadparwaiz1/cmp_luasnip", commit = "" }
+    use { "hrsh7th/cmp-cmdline", commit = "" }
+    use { "hrsh7th/cmp-nvim-lsp", commit = "" }
+    use { 'hrsh7th/cmp-nvim-lua', commit = "" }
+    use { 'jezda1337/cmp_bootstrap', commit = "" }
 
     -- snippets
-    use("L3MON4D3/LuaSnip")
-    use("rafamadriz/friendly-snippets")
+    use { "L3MON4D3/LuaSnip", commit = "" }
+    use { "rafamadriz/friendly-snippets", commit = "" }
 
-    use('yaegassy/coc-blade')
-    use('https://github.com/jwalton512/vim-blade')
+    use { 'yaegassy/coc-blade', commit = "" }
+    use { 'https://github.com/jwalton512/vim-blade', commit = "" }
 
     -- fix error while opening directory in lir
-    use("tpope/vim-vinegar")
+    use { "tpope/vim-vinegar", commit = "" }
 
     -- git
-    use("tpope/vim-fugitive")
+    use { "tpope/vim-fugitive", commit = "" }
     use {
         'lewis6991/gitsigns.nvim',
+        commit = ""
         -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
     }
 
     -- use { "akinsho/toggleterm.nvim", tag = '*', config = function()
     --     require("toggleterm").setup()
     -- end }
-    use('voldikss/vim-floaterm')
-    use('ptzz/lf.vim') -- used with floaterm
+    use { 'voldikss/vim-floaterm', commit = "" }
+    use { 'ptzz/lf.vim', commit = "" } -- used with floaterm
 
     -- tabline
-    use("akinsho/bufferline.nvim")
-    use("tiagovla/scope.nvim") -- This plugin scopes buffers to tabs cleaning up tabline plugins like bufferline.nvim.
-    use("moll/vim-bbye")
-    use("jose-elias-alvarez/buftabline.nvim")
+    use { "akinsho/bufferline.nvim", commit = "" }
+    use { "tiagovla/scope.nvim", commit = "" } -- This plugin scopes buffers to tabs cleaning up tabline plugins like bufferline.nvim.
+    use { "moll/vim-bbye", commit = "" }
+    use { "jose-elias-alvarez/buftabline.nvim", commit = "" }
 
     use {
         'j-hui/fidget.nvim',
-        tag = 'legacy'
+        tag = 'legacy',
+        commit = ""
     }
 end)
