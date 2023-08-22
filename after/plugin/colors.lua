e4d67dbdff14b2688fa523d7a4f799d6928250cd
prospero <g0lden_vo1d@protonmail.com>
2023-08-22 13:43:31 +0200

    sync

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,15 @@
 vim.o.background = "dark"
 vim.cmd.colorscheme("retrobox")
 
+if vim.g.colors_name == "retrobox" then
+    vim.cmd.highlight("DiffAdd cterm=reverse ctermfg=142 ctermbg=NONE gui=NONE guifg=#b8bb26 guibg=NONE")
+    vim.cmd.highlight("DiffDelete cterm=reverse ctermfg=142 ctermbg=NONE gui=NONE guifg=#b8bb26 guibg=NONE")
+    vim.cmd.highlight("DiffChange cterm=reverse ctermfg=107 ctermbg=NONE gui=NONE guifg=#8ec07c guibg=NONE")
+
+    vim.cmd.highlight("TelescopeSelection ctermfg=234 ctermbg=109 guifg=#ffffff guibg=#3d2004")
+    vim.cmd.highlight("TelescopeMatching ctermfg=208 guifg=#ffab66")
+end
+
 -- vim.cmd [[
 --     let g:material_theme_style = 'ocean-community'
 --     let g:material_terminal_italics = 1
@@ -21,19 +30,20 @@ vim.cmd.highlight("StatusLine guibg=NONE ctermbg=NONE")
 vim.cmd.highlight("SignColumn guibg=NONE ctermbg=NONE")
 vim.cmd.highlight("MsgArea guibg=NONE ctermbg=NONE")
 
-vim.cmd.highlight("VimwikiHeader1 guifg=#00838f gui=bold")
-vim.cmd.highlight("VimwikiHeader2 guifg=#00bcd4 gui=bold")
-vim.cmd.highlight("VimwikiHeader3 guifg=#4dd0e1 gui=bold")
-vim.cmd.highlight("VimwikiHeader4 guifg=#94FBEF gui=bold")
-vim.cmd.highlight("VimwikiHeader5 guifg=#94FBEF gui=bold")
-vim.cmd.highlight("VimwikiHeader6 guifg=#94FBEF gui=bold")
+-- vim.cmd.highlight("VimwikiHeader1 guifg=#00838f gui=bold")
+-- vim.cmd.highlight("VimwikiHeader2 guifg=#00bcd4 gui=bold")
+-- vim.cmd.highlight("VimwikiHeader3 guifg=#4dd0e1 gui=bold")
+-- vim.cmd.highlight("VimwikiHeader4 guifg=#94FBEF gui=bold")
+-- vim.cmd.highlight("VimwikiHeader5 guifg=#94FBEF gui=bold")
+-- vim.cmd.highlight("VimwikiHeader6 guifg=#94FBEF gui=bold")
 
-vim.cmd.highlight("VimwikiLink guifg=#c5e1a5 gui=underline")
-vim.cmd.highlight("VimwikiHR guifg=#aed581 gui=bold")
-vim.cmd.highlight("VimwikiList guifg=#71E885 gui=bold")
-vim.cmd.highlight("VimwikiListTodo guifg=#9f9f9f gui=bold")
+-- vim.cmd.highlight("VimwikiLink guifg=#c5e1a5 gui=underline")
+-- vim.cmd.highlight("VimwikiHR guifg=#aed581 gui=bold")
+-- vim.cmd.highlight("VimwikiList guifg=#71E885 gui=bold")
+-- vim.cmd.highlight("VimwikiListTodo guifg=#9f9f9f gui=bold")
 
 -- vim.api.nvim_set_hl(0, "VimwikiList", { link = "VimwikiHeader1" })
 -- vim.cmd.highlight("VimwikiList guifg=#ff0000 gui=bold")
 
 vim.cmd.highlight("FidgetTitle ctermfg=110 guifg=#6cb6eb")
+vim.cmd.highlight("FidgetTask ctermfg=239 guifg=#ffffff")
