243a230938ef41f53bff7590e52c4b12678685d2
prospero <g0lden_vo1d@protonmail.com>
2023-09-16 16:58:01 +0200

    sync

--- after/plugin/linter.lua
+++ after/plugin/linter.lua
@@ -3,11 +3,15 @@ local phpcs = require('lint.linters.phpcs')
 
 phpcs.args = {
     '-q',
+    -- '--standard=PSR12',
     '--standard=PSR12',
     '--exclude=Generic.Commenting.Todo,Squiz.PHP.CommentedOutCode',
     '--report=json',
-    '-'
+    '-', -- need `-` at the end for stdin support
 }
+
+phpcs.cmd = "phpcs"
+-- phpcs.cmd = os.getenv('HOME') .. '/.local/share/nvim/mason/bin/php-cs-fixer'
 
 lint.linters_by_ft = {
     sh = { 'shellcheck' },
@@ -17,7 +21,6 @@ lint.linters_by_ft = {
 
 vim.api.nvim_create_autocmd({ "BufWritePost", "TextChanged" }, {
     callback = function()
-        phpcs.cmd = os.getenv('HOME') .. '/.local/share/nvim/mason/bin/php-cs-fixer'
         require('lint').try_lint()
     end,
 })
