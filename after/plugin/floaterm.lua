2ca370997b439d90eabab7bb99a3f38f96cb882b
prospero <g0lden_vo1d@protonmail.com>
2023-09-30 11:28:25 +0200

    sync

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -2,8 +2,8 @@ vim.keymap.set("n", "<C-t>", ':FloatermToggle<CR>')
 vim.keymap.set("n", "<C-e>", '<C-\\><C-n>:Lf<CR>')
 vim.keymap.set("n", "<C-g>", '<C-\\><C-n>:FloatermNew lazygit<CR>')
 vim.keymap.set("t", "<C-t>", '<C-\\><C-n>:FloatermToggle<CR>')
-vim.keymap.set("t", "<C-n>", '<C-\\><C-n>:FloatermNext<CR>')
-vim.keymap.set("t", "<C-p>", '<C-\\><C-n>:FloatermPrev<CR>')
+vim.keymap.set("t", "<C-j>", '<C-\\><C-n>:FloatermNext<CR>')
+vim.keymap.set("t", "<C-k>", '<C-\\><C-n>:FloatermPrev<CR>')
 
 vim.cmd [[
     let g:floaterm_width = 130
