b1c81e360882198385291f6b1884dfca5161e8d7
prosp3ro <g0lden_vo1d@protonmail.com>
2023-12-13 19:09:06 +0100

    sync

--- after/plugin/lazygit.lua
+++ after/plugin/lazygit.lua
@@ -7,7 +7,7 @@ keymap("n", "<C-g>", ":LazyGit<CR>")
 -- keymap("n", "<leader>gc", ":LazyGitFilterCurrentFile<CR>")
 
 g.lazygit_floating_window_winblend = 0 -- transparency of floating window
-g.lazygit_floating_window_scaling_factor = 0.7 -- scaling factor for floating window
+g.lazygit_floating_window_scaling_factor = 0.85 -- scaling factor for floating window
 g.lazygit_floating_window_border_chars = {'╭','─', '╮', '│', '╯','─', '╰', '│'} -- customize lazygit popup window border characters
 g.lazygit_floating_window_use_plenary = 0 -- use plenary.nvim to manage floating window if available
 g.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
