e1a74d50646746cd11e116c5a6b579f3a8e1f9cf
prosp3ro <g0lden_vo1d@protonmail.com>
2024-01-17 10:26:55 +0100

    sync

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -13,8 +13,8 @@ local keymap_opts = { silent = true }
 --     keymap_opts
 -- )
 
-vim.keymap.set("n", "<C-p>", ":Telescope git_files hidden=true no_ignore=true<CR>", keymap_opts)
-vim.keymap.set("n", "<leader>ff", ":Telescope find_files hidden=true no_ignore=true<CR>", keymap_opts)
+vim.keymap.set("n", "<leader>fg", ":Telescope git_files hidden=true no_ignore=true<CR>", keymap_opts)
+vim.keymap.set("n", "<leader>fa", ":Telescope find_files hidden=true no_ignore=true<CR>", keymap_opts)
 vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>", keymap_opts)
 vim.keymap.set("n", "<leader>fd", ":Telescope diagnostics<CR>", keymap_opts)
 
