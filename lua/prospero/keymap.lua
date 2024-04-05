482845acee7263290b0c5bf6c02106642f624449
prosp3ro <g0lden_vo1d@protonmail.com>
2024-04-05 14:56:13 +0200

    sync

--- lua/prospero/keymap.lua
+++ lua/prospero/keymap.lua
@@ -95,7 +95,7 @@ keymap("n", "L", "$")
 
 keymap("i", "<C-h>", "<C-o>^")
 keymap("i", "<C-l>", "<C-o>$")
-keymap("i", "<M-l>", "<C-o>f;")
+-- keymap("i", "<M-l>", "<C-o>f;")
 keymap('i', ';', '<Esc>A;')
 keymap('i', '<M-;>', ';') -- ctrl won't work
 
