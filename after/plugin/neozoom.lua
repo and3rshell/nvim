a802a467504c8cac042c5cc917e261e0e0873f8e
prosp3ro <g0lden_vo1d@protonmail.com>
2024-05-05 10:59:11 +0200

    sync

--- after/plugin/neozoom.lua
+++ after/plugin/neozoom.lua
@@ -17,11 +17,11 @@ neozoom.setup {
             -- NOTE: omit `top`/`left` to center the floating window vertically/horizontally.
             -- top = 0,
             -- left = 0.17,
-            width = 150,
-            height = 0.85,
+            width = 215,
+            height = 0.9,
         },
         -- NOTE: check :help nvim_open_win() for possible border values.
-        border = 'thicc', -- this is a preset, try it :)
+        border = "single", -- this is a preset, try it :)
     },
     presets = {
         {
