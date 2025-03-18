0e381d9771e8916ca20c32188b2c2dc1dfd9d525
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2025-03-18 18:58:02 +0100

    nvim rewrite

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,28 +1,27 @@
 return {
     "ThePrimeagen/harpoon",
     enabled = false,
+    branch = "harpoon2",
     config = function()
         local harpoon = require "harpoon"
-        local mark = require "harpoon.mark"
-        local ui = require "harpoon.ui"
-        harpoon:setup()
+        harpoon:setup{
+            menu = {
+                width = 80,
+                height = 15
+            }
+        }
 
-        vim.keymap.set("n", "<leader>a", mark.add_file)
-        vim.keymap.set("n", "m", ui.toggle_quick_menu)
-        -- vim.keymap.set("n", "m", "<cmd>lua require('telescope').extensions.harpoon.marks(require('telescope.themes').get_dropdown{previewer = false, initial_mode='normal', prompt_title='Harpoon'})<cr>")
+        vim.keymap.set("n", "<space>a", function()
+            harpoon:list():add()
+        end)
+        vim.keymap.set("n", "m", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end)
 
         for _, idx in ipairs { 1, 2, 3, 4, 5 } do
             vim.keymap.set("n", string.format("<space>%d", idx), function()
                 harpoon:list():select(idx)
             end)
         end
-
-        vim.keymap.set("n", 'M', function()
-            ui.toggle_quick_menu()
-            -- wait for the menu to open
-            vim.defer_fn(function()
-                vim.fn.feedkeys('/')
-            end, 50)
-        end, { noremap = true })
     end,
 }
