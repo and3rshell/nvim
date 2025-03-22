26d453f1982537768524d8d954812b7fb57cfbdd
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2025-03-22 19:03:52 +0100

    nvim rewrite

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,22 +1,26 @@
 return {
     "ThePrimeagen/harpoon",
-    enabled = false,
     branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+        menu = {
+            width = 80,
+            height = 15
+        },
+        settings = {
+            save_on_toggle = true,
+        },
+    },
     config = function()
         local harpoon = require "harpoon"
-        harpoon:setup{
-            menu = {
-                width = 80,
-                height = 15
-            }
-        }
+        local extensions = require("harpoon.extensions");
+        harpoon:setup()
 
-        vim.keymap.set("n", "<space>a", function()
-            harpoon:list():add()
-        end)
-        vim.keymap.set("n", "m", function()
-            harpoon.ui:toggle_quick_menu(harpoon:list())
-        end)
+        harpoon:extend(extensions.builtins.highlight_current_file())
+        harpoon:extend(extensions.builtins.navigate_with_number());
+
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        vim.keymap.set("n", "m", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
         for _, idx in ipairs { 1, 2, 3, 4, 5 } do
             vim.keymap.set("n", string.format("<space>%d", idx), function()
--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,13 +2,101 @@ return {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        -- CustomOilBar = function()
-        --     local path = vim.fn.expand "%"
-        --     path = path:gsub("oil://", "")
-        --
-        --     return "  " .. vim.fn.fnamemodify(path, ":.")
-        -- end
+        vim.keymap.set("n", "-", "<CMD>Oil<CR>")
+        vim.keymap.set("n", "<leader>e", "<CMD>Oil --float<CR>")
+        -- vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>")
 
-        require "custom.oil"
-  end,
+        require("oil").setup({
+            columns = { "icon", },
+            keymaps = {
+                ["g?"] = { "actions.show_help", mode = "n" },
+                ["<CR>"] = "actions.select",
+                ["<space>v"] = { "actions.select", opts = { vertical = true } },
+                ["<space>x"] = { "actions.select", opts = { horizontal = true } },
+                ["<space>t"] = { "actions.select", opts = { tab = true } },
+                ["q"] = "actions.close",
+                ["r"] = "actions.refresh",
+                ["<C-p>"] = "actions.preview",
+                ["<C-c>"] = { "actions.close", mode = "n" },
+                ["<C-l>"] = "actions.refresh",
+                ["-"] = { "actions.parent", mode = "n" },
+                ["_"] = { "actions.open_cwd", mode = "n" },
+                ["`"] = { "actions.cd", mode = "n" },
+                ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+                ["gs"] = { "actions.change_sort", mode = "n" },
+                ["gx"] = "actions.open_external",
+                ["zh"] = { "actions.toggle_hidden", mode = "n" },
+                ["g\\"] = { "actions.toggle_trash", mode = "n" },
+            },
+            use_default_keymaps = true,
+            view_options = {
+                show_hidden = true,
+                -- This function defines what is considered a "hidden" file
+                is_hidden_file = function(name, bufnr)
+                    local m = name:match("^%.")
+                    return m ~= nil
+                end,
+                -- This function defines what will never be shown, even when `show_hidden` is set
+                is_always_hidden = function(name, bufnr)
+                    return false
+                end,
+                -- Sort file names with numbers in a more intuitive order for humans.
+                -- Can be "fast", true, or false. "fast" will turn it off for large directories.
+                natural_order = "fast",
+                -- Sort file and directory names case insensitive
+                case_insensitive = false,
+                sort = {
+                    -- sort order can be "asc" or "desc"
+                    -- see :help oil-columns to see which columns are sortable
+                    { "type", "asc" },
+                    { "name", "asc" },
+                },
+                -- Customize the highlight group for the file name
+                highlight_filename = function(entry, is_hidden, is_link_target, is_link_orphan)
+                    return nil
+                end,
+            },
+            git = {
+                -- Return true to automatically git add/mv/rm files
+                add = function(path)
+                    return false
+                end,
+                mv = function(src_path, dest_path)
+                    return false
+                end,
+                rm = function(path)
+                    return false
+                end,
+            },
+            -- Configuration for the floating window in oil.open_float
+            float = {
+                -- Padding around the floating window
+                padding = 10,
+                -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+                max_width = 150,
+                max_height = 150,
+                border = "rounded",
+                win_options = {
+                    winblend = 0,
+                },
+                -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+                get_win_title = nil,
+                -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+                preview_split = "auto",
+                -- This is the config that will be passed to nvim_open_win.
+                -- Change values here to customize the layout
+                override = function(conf)
+                    return conf
+                end,
+            },
+            -- Configuration for the floating SSH window
+            ssh = {
+                border = "rounded",
+            },
+            -- Configuration for the floating keymaps help window
+            keymaps_help = {
+                border = "rounded",
+            },
+        })
+    end,
 }
