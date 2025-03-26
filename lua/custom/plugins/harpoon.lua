25cac59a5e80336320a4aae4504026ae3f4233ac
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2025-03-26 14:44:05 +0100

    sync

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -20,7 +20,16 @@ return {
         harpoon:extend(extensions.builtins.navigate_with_number());
 
         vim.keymap.set("n", "<space>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "m", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "m", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list(), {
+                ui_max_width = 85,
+                ui_width_ratio = 0.5,
+                ui_min_width = 40,
+                border = "rounded",
+                title = "Harpoon",
+                height_in_lines = 15
+            })
+        end)
 
         for _, idx in ipairs { 1, 2, 3, 4, 5 } do
             vim.keymap.set("n", string.format("<space>%d", idx), function()
