23235ba6fb5aa382c0dd629f611fda092192dbd1
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2025-05-16 15:22:41 +0200

    sync

--- after/ftplugin/php.lua
+++ after/ftplugin/php.lua
@@ -1,1 +1,10 @@
 vim.opt.commentstring = "// %s"
+
+vim.cmd([[
+    " command! FuncSearch execute "/\\%(protected\\|public\\|private\\)\\zs.*function.*"
+    command! FuncSearch execute "/\\%(protected\\|public\\|private\\)\.*function.*"
+]])
+
+vim.keymap.set("n", "<leader>/F", ":FuncSearch<CR>")
+vim.keymap.set("n", "<leader>/f", "/fun.*")
+vim.keymap.set("n", "<leader>/c", "/case\\|default.*:$<CR>")
