b30226d8d71c7c062d805de06ee35b79800d457e
Andershell <contact@andershell.art>
2025-10-01 21:15:48 +0200

    sync

--- lua/custom/plugins/bufdel.lua
+++ lua/custom/plugins/bufdel.lua
@@ -1,14 +1,43 @@
 return {
-  "ojroques/nvim-bufdel",
+  "kazhala/close-buffers.nvim",
   config = function()
-    require("bufdel").setup({
-      next = "tabs",
-      quit = false, -- quit Neovim when last buffer is closed
+    local close_buffers = require("close_buffers")
+
+    close_buffers.setup({
+      preserve_window_layout = { "this" },
+      next_buffer_cmd = function(windows)
+        local ok, bufferline = pcall(require, "bufferline")
+
+        if ok then
+          bufferline.cycle(1)
+        else
+          vim.cmd("bnext")
+        end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+
+        for _, window in ipairs(windows) do
+          if vim.api.nvim_win_is_valid(window) then
+            vim.api.nvim_win_set_buf(window, bufnr)
+          end
+        end
+      end,
     })
 
-    vim.keymap.set("n", "<space>dd", "<cmd>BufDel<cr>")
-    vim.keymap.set("n", "<space>df", "<cmd>BufDel!<cr>")
-    vim.keymap.set("n", "<space>do", "<cmd>:BufDelOthers<cr><cr>")
-    vim.keymap.set("n", "<space>da", "<cmd>:BufDelAll<cr><cr>")
+    vim.keymap.set("n", "<space>dd", function()
+      close_buffers.delete({ type = "this" })
+    end, { silent = true })
+
+    vim.keymap.set("n", "<space>df", function()
+      close_buffers.delete({ type = "this", force = true })
+    end, { silent = true })
+
+    vim.keymap.set("n", "<space>do", function()
+      close_buffers.delete({ type = "others" })
+    end, { silent = true })
+
+    vim.keymap.set("n", "<space>da", function()
+      close_buffers.delete({ type = "all" })
+    end, { silent = true })
   end,
 }
